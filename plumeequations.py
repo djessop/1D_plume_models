9dc464851148e3f259d4dde62f24ef609343f0ae
David Jessop <d.jessop@opgc.fr>
2024-02-12 16:42:02 +0100

    Reworked plume equations to {b, u, phi, s} set

--- plumeequations.py
+++ plumeequations.py
@@ -15,13 +15,12 @@ plumeequations.py
     dq/dx = 2 * alphae * np.sqrt(m)
     dm/dx = b**2 * g' = q**2 * g' / m = f0 * q / m
 """
-
 from scipy.integrate import solve_ivp
+from scipy.optimize import newton
 from SW_Properties.SW_Density import (rho_sw, drho_sw_ds,
                                       drho_sw_dT, rho_plain)
 
 import numpy as np
-
 
 
 def derivs(x, V, params):
@@ -115,7 +114,29 @@ def rho_amb(x, V, params):
     return rho_a
 
 
+def ambient_salinity(x, V, params):
+    rho_a = rho_amb(x, V, params)
+
+    T, s0 = 20., 20.
+
+    def target(s, x, T):
+        return rho_sw(T, s) - x
+
+    if type(rho_a) == np.ndarray or type(rho_a) == list:
+        amb_salinity = []
+        for target_density in rho_a:
+            amb_salinity.append(newton(target, x0=s0,
+                                       args=(target_density, T)))
+    else:
+        amb_salinity = newton(target, x0=s0, args=(rho_a, T))
+
+    return amb_salinity
+
+
 def woods2010_derivs(x, V, params=(.1, 1)):
+    '''
+    
+    '''
     alpha, N2 = params
 
     Q, M, F = np.array(V)  # structure of soln from solve_ivp is 3xN
@@ -127,8 +148,85 @@ def woods2010_derivs(x, V, params=(.1, 1)):
     return dVdx
 
 
-def params_from_dict(expt_conds):
+def derivs_base_parameters(x, V, params=(.1, 1)):
+    '''
+    The following equations give exactly the same solution as "woods2010_derivs"
+    except rather than using the {Q, M, F} set of parameters, they directly use 
+    the {b, u, gp} set.
+    '''
+    alphae, N2 = params
+    b, u, gp   = np.array(V)
 
+    dVdx = np.array([2*alphae - 1/2 * b * gp / u**2,  # N.B. b * gp / u**2 = Ri
+                     gp / u   - 2*alphae * u / b,
+                     -N2      - 2*alphae * gp / b])
+    return dVdxw
+
+
+def derivs_buphis(x, V, params=(.1, 0, None)):
+    '''
+    The following equations use the {b, u, phi, s} set of parameters, where the 
+    equations for dphi/dx and ds/dx come from conservation of particle and salt
+    mass.
+    '''
+    alphae, us, *_ = params
+    b, u, phi, s   = np.array(V)
+
+    sa = ambient_salinity(x, V, params)
+    gp = gprimed_buphis(x, V, params)
+
+    dVdx = np.array([2*alphae - 1/2 * b * gp / u**2,  # N.B. b * gp / u**2 = Ri
+                     gp / u   - 2*alphae * u / b,
+                     2 / b * (us / u - alphae * phi),
+                     2 / b * alphae * (sa - s)])
+                     
+    return dVdx
+
+
+def gprimed_buphis(x, V, params):
+    rho_0 = params[4]
+    rho_w = params[5]
+    rho_p = params[6]
+    V0    = params[7]
+    g     = params[8]
+    
+    rho_a = rho_amb(x, V, params)
+    rho   = rho_bulk_buphis(x, V, params)
+
+    return (rho_a - rho) / rho * g
+
+
+def rho_bulk_buphis(x, V, params):
+    '''
+    rho_b = (1 - phi) * rho_w + phi * rho_s
+   
+    but as rho_w(z) = Q_0/Q(z) * (1 - phi_0) / (1 - phi(z)) * rho_{w,0},
+    => rho_b = (1 - phi_0) * Q_0/Q(z) * rho_{w,0} + phi(z) * rho_s
+    
+    '''
+    T = 20.
+    b, u, phi, s = V
+
+    rho_s = params[6]
+
+    return (1 - phi) * rho_sw(T, s) + phi * rho_s
+
+
+def convert_bugp_qmf(x, V, params=(.1, 1, None)):
+    b, u, gp = np.array(V)
+    q, m, f  = b**2 * u, b**2 * u**2, b**2 * u * gp
+
+    return np.array([q, m, f])
+
+
+def convert_qmf_bugp(x, V, params=(.1, 1, None), include_pi=False):
+    q, m, f  = np.array(V)
+    b, u, gp = q / np.sqrt(m), m / q, f / q
+
+    return np.array([b, u, gp])
+
+
+def params_from_dict(expt_conds):
     params = []
     if 'alpha_e' in expt_conds:
         params.append(expt_conds['alpha_e'])
@@ -165,8 +263,11 @@ def params_from_dict(expt_conds):
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
+    import sys
 
-    plot = True
+    plot_soln = False
+    if len(sys.argv) > 1:
+        plot_soln = sys.argv[1]
 
     alpha_e = .1      # entrainement coefficient
     N2      = .684    # buoyancy frequency/[Hz]
@@ -174,76 +275,68 @@ if __name__ == '__main__':
     g       = 981     # gravitational field strength/[cm/s2]
     t0      =  0.     # vent height/[cm]
     t1      = 50.     # max height of domain/[cm]
-    phi_0   = 0.050   # particle volume fraction at source/[-]
+    phi0    = 0.050   # particle volume fraction at source/[-]
     rho_w   =  .9998  # density of water within plume/[g/cm3]
-    rho_0   = 1.0010  # ambient density at source/[g/cm3]
+    rho_0   = 1.0100  # ambient density at source/[g/cm3]
     rho_p   = 2.5     # density of particles/[g/cm3]
-    r0 =    .4        # source radius/[cm]
+    b0 =    .4        # source radius/[cm]
     Q0 =  30.         # source volume flux/[cm3/s]
-    u0 = Q0 / (np.pi * r0**2) # source velocity/[cm/s]
+    u0 = Q0 / (np.pi * b0**2) # source velocity/[cm/s]
     us = 0            # settling velocity/[cm/s]
+    s0 = 0.           # salinity as source/[g/kg] or [ppt]
     M0 = Q0 * u0      # source momentum flux/[cm4/s2]
-    P0 = phi_0 * Q0   # source particle volume flux/[cm3/s]
-    V0 = (Q0, M0, P0)
+    P0 = phi0 * Q0    # source particle volume flux/[cm3/s]
+    V0 = (b0, u0, phi0, s0) # (Q0, M0, P0)
     #sol = solve_ivp(derivs, [t0, t1], V0)
     stratification_type = 'gradient'  # 'gradient' or step height value
-    gradient = -1.7333e-4
+    gradient = -2e-4
     params   = (alpha_e, us, stratification_type, 
                 gradient, rho_0, rho_w, rho_p, V0, g)
-    rho_b    = rho_bulk(0, V0, params)  # source bulk density/[g/cm3]
-    gp0      = gprimed(0, V0, params)   # reduced gravity at source/[cm/s2]
+    rho_b    = rho_bulk_buphis(0, V0, params)  # source bulk density/[g/cm3]
+    gp0      = gprimed_buphis(0, V0, params)   # g' at source/[cm/s2]
 
     expt_conds = {'alpha_e': alpha_e,
                   'N2': N2,
                   'stratification type': stratification_type,
                   'gradient': gradient,
-                  'phi_0': phi_0,
-                  'rho_b': rho_b,
-                  'rho_0': rho_0,
                   'gp0': gp0,
-                  'r0': r0,
-                  'Q0': Q0,
+                  'b0': b0,
                   'u0': u0,
                   'us': us,
+                  'phi0': phi0,
+                  's0': s0,
+                  'rho_b': rho_b,
+                  'rho_0': rho_0,
+                  'Q0': Q0,
                   'V0': V0}
 
     print(f'alphae = {expt_conds["alpha_e"]}')
-    print(f'N2     = {expt_conds["N2"]}')
-    print(f'phi_0  = {expt_conds["phi_0"]}')
+    print(f'b0     = {expt_conds["b0"]}')
+    print(f'u0     = {expt_conds["u0"]:.3f}')
+    print(f'us     = {expt_conds["us"]:.3f}')
+    print(f'phi0   = {expt_conds["phi0"]}')
+    print(f's0     = {expt_conds["s0"]}')
+    print(f'rho_0  = {expt_conds["rho_0"]:.3f}')
     print(f'rho_b  = {expt_conds["rho_b"]:.3f}')
     print(f'gp0    = {expt_conds["gp0"]:.3f}')
     print(f'Q0     = {expt_conds["Q0"]}')
-    print(f'u0     = {expt_conds["u0"]:.3f}')
-    print(f'us     = {expt_conds["us"]:.3f}')
 
-    n_steps = 501
+    n_steps = 1001
     t  = np.linspace(t0, t1, n_steps)
-    sol = solve_ivp(derivs, [t0, t1], V0, t_eval=t, args=(params,))
-
+    #sol = solve_ivp(derivs, [t0, t1], V0, t_eval=t, args=(params,))
+    sol = solve_ivp(derivs_buphis, [t0, t1], V0, t_eval=t, args=(params,))
+    
     if plot_soln:
         # Plot solution
         plt.close('all')
         plt.plot(sol.y.T, sol.t, '-')
-        plt.legend(('Q', 'M', 'P'))
+        plt.legend((r'$b$', r'$\bar{u}$', r'$\phi$', r'$s$'))
         plt.grid('both')
 
-        Q, M, P  = sol.y
-        b, u, phi = Q / np.sqrt(np.pi * M), M / Q, P / Q
-        gp    = gprimed(sol.t, sol.y, params)
-        rho_a = rho_amb(sol.t, sol.y, params)
-        rho_b = rho_bulk(sol.t, sol.y, params)
-
-        W  = np.vstack([b, u/u0, phi,
-                        #gp,
-                        rho_a, rho_b])
-        W0 = W.T[0]
-        W0inv = list(map(lambda x: 1/x, W0))
-        plt.subplots()
-        plt.plot(W.T * W0inv, sol.t, '-')
         plt.xlabel(r'Plume parameter')
         plt.ylabel(r'Altitude, $x$')
-        plt.legend((r'$b/b_0$', r'$\bar{u}/\bar{u}_0$', r'$\phi/\phi_0$',
-                    r'$\rho_a/\rho_{a,0}$', r'$\rho_b/\rho_{b,0}$'))
-        plt.grid('both')
+        # plt.legend((r'$b/b_0$', r'$\bar{u}/\bar{u}_0$', r'$\phi/\phi_0$',
+        #             r'$\rho_a/\rho_{a,0}$', r'$\rho_b/\rho_{b,0}$'))
+        # plt.grid('both')
 
     print(f"Plume predicted to reach {sol.t[-1]:.3f} units)")
